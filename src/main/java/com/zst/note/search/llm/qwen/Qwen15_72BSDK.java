36a82b7de6432a9c343efcfb2ad9ca86e8cbd853
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-06-06 23:43:11 +0800

    feat: 代码临时提交

---
+++ src/main/java/com/zst/note/search/llm/qwen/Qwen15_72BSDK.java
@@ -0,0 +1,14 @@
+package com.zst.note.search.llm.qwen;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 通义千问开源版本72b参数模型-SDK
+ */
+@Component
+@Slf4j
+public class Qwen15_72BSDK {
+    private static final String MODEL_NAME = "qwen1.5-72b-chat";
+
+}
